e09403b6778530226a6950ce6aa1f7069e278095
IlyaChuvilko <89748002+ChuvilkoDEV@users.noreply.github.com>
2022-09-11 21:47:11 +0300

    Перенос файла Programming Basics

---
+++ ASD/Лабораторная №1/cmake-build-debug/CMakeFiles/_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_1.dir/main.c.obj"
+  "_1.exe"
+  "_1.exe.manifest"
+  "_1.pdb"
+  "lib_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ASD/Лабораторная №1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Задание 1
+enum seeson {
+    red,
+    yellow,
+    green
+};
