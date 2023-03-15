fa6e7355f9525489443ae0c151e91e9937048600
ChuvilkoDEV <ilyasutton@gmail.com>
2023-03-15 11:43:28 +0300

    мерж

--- VicMat/Lab1/Code/main.cpp
+++ VicMat/Lab1/Code/main.cpp
@@ -4,14 +4,12 @@
 int main() {
   //matrix m = {{1, 1,2,3, 5}, {1, 4, 5, 6, 6}, {2, 7, 8, 9, 7}, {2, 4, 1, 4, 4}};
   //matrix m = {{3, -2, 2, 10}, {2, 1, -4, -7}, {2, 2, -6, -12}};
-  matrix m = {{3, 5, 1}, {1, 0, 2}, {2, 2, -1}};
+  matrix m = {{3, 2, 21}, {6, 4, 42}};
 
   Matrix M1(m);
-  Matrix M2(m);
-  M1.inverse();
 
-  Matrix res = Matrix::Multiplication(M1, M2);
-  res.Output();
+  M1.forwardGauss();
+  M1.Output();
 
   return 0;
 }
