daec9621cefa511f2743bfa44b18692df81b9318
IlyaChuvilko <89748002+ChuvilkoDEV@users.noreply.github.com>
2023-03-26 23:35:11 +0300

    ТИ: 2 лаба

---
+++ TerInf/2lab/Code/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int length;
+
+struct SymbolCode{
+  char symbol;
+  int count;
+  float probability;
+  string code;
+};
+
+int main() {
+  string message;
+  cin >> message;
+  cout << message;
+  return 0;
+}
---
+++ TerInf/2lab/Code1/cmake-build-debug/CMakeFiles/Code1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code1.dir/main.cpp.obj"
+  "Code1.exe"
+  "Code1.exe.manifest"
+  "Code1.pdb"
+  "libCode1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Code1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
