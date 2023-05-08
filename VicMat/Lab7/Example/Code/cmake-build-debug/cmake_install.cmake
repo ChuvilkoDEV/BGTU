1ce4a192c5454127167228c093983c09469edc1d
IlyaChuvilko <ilyasutton@gmail.com>
2023-05-08 20:43:56 +0300

    ВМ: 7 лаба

--- VicMat/Lab7/Example/Code/cmake-build-debug/cmake_install.cmake
+++ VicMat/Lab7/Example/Code/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/dmedv/Desktop/Учеба/4 семестр/Вычислительная математика/Лабораторные работы/ЛР7/Code
+# Install script for directory: C:/BGTU/BGTU/VicMat/Lab7/Example/Code
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Code")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/dmedv/Desktop/Учеба/4 семестр/Вычислительная математика/Лабораторные работы/ЛР7/Code/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/BGTU/BGTU/VicMat/Lab7/Example/Code/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- VicMat/Lab7/Example/Code/main.cpp
+++ VicMat/Lab7/Example/Code/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "cmath"
+#include "windows.h"
 
 using namespace std;
 
 struct Point {
-    float x1;
-    float x2;
+  float x1;
+  float x2;
+  void output() {
+    cout << x1 << ' ' << x2 << '\n';
+  }
 };
 
 typedef float func(Point point);
@@ -13,54 +17,56 @@ typedef float func(Point point);
 typedef Point grad(Point point);
 
 float targetFunction(Point point) {
-    return pow(point.x1, 2) + pow(point.x2, 2) - point.x1 * point.x2 + 2 * point.x1 - 4 * point.x2;
+  return pow(point.x1, 2) + pow(point.x2, 2) + point.x1 * point.x2 - point.x1 - 2 * point.x2 + 5;
 }
 
 Point gradient(Point point) {
-    return {2 * point.x1 - point.x2 + 2, 2 * point.x2 - point.x1 - 4};
+  return {2 * point.x1 + point.x2 - 1, 2 * point.x2 + point.x1 - 2};
 }
 
 bool findMinPoint(func targetFunction, grad gradient, Point &point, float &minValue, float eps, int maxIterations,
-                   float alpha, float betta, float gamma) {
-    float precision = eps * 2;
-    int iteration = 0;
+                  float alpha, float betta, float gamma) {
+  float precision = eps * 2;
+  int iteration = 0;
 
-    while (precision > eps && iteration < maxIterations) {
-        Point currentGradient = gradient(point);
-        float gradientModuleSquared = pow(currentGradient.x1, 2) + pow(currentGradient.x2, 2);
-        Point newPoint;
+  while (precision > eps && iteration < maxIterations) {
+    Point currentGradient = gradient(point);
+    float gradientModuleSquared = pow(currentGradient.x1, 2) + pow(currentGradient.x2, 2);
+    Point newPoint;
 
-        int i = 0;
-        bool equationIsValid = false;
-        while (!equationIsValid) {
-            i++;
-            newPoint = {(float) (point.x1 - alpha * pow(gamma, i) * currentGradient.x1),
-                        (float) (point.x2 - alpha * pow(gamma, i) * currentGradient.x2)};
+    int i = 0;
+    bool equationIsValid = false;
+    while (!equationIsValid) {
+      i++;
+      newPoint = {(float) (point.x1 - alpha * pow(gamma, i) * currentGradient.x1),
+                  (float) (point.x2 - alpha * pow(gamma, i) * currentGradient.x2)};
 
-            equationIsValid = targetFunction(newPoint) - targetFunction(point) <
-                              -betta * alpha * pow(gamma, i) * gradientModuleSquared;
-        }
-
-        precision = abs(targetFunction(newPoint) - targetFunction(point));
-        point = newPoint;
-        iteration++;
+      equationIsValid = targetFunction(newPoint) - targetFunction(point) <
+                        -betta * alpha * pow(gamma, i) * gradientModuleSquared;
     }
 
-    minValue = targetFunction(point);
-    return precision < eps;
+    precision = abs(targetFunction(newPoint) - targetFunction(point));
+    point = newPoint;
+    point.output();
+    iteration++;
+  }
+
+  minValue = targetFunction(point);
+  return precision < eps;
 }
 
 
 int main() {
-    Point point = {1, 1};
-    float minValue;
-    bool isSuccessful = findMinPoint(targetFunction, gradient, point, minValue, 0.0001, 100, 1, 0.5, 0.5);
+  SetConsoleOutputCP(CP_UTF8);
+  Point point = {1, 1};
+  float minValue;
+  bool isSuccessful = findMinPoint(targetFunction, gradient, point, minValue, 0.00001, 100, 1, 0.5, 0.5);
 
-    if (isSuccessful) {
-        cout << "Необходимая точность достигнута";
-    } else {
-        cout << "Необходимая точность не достигнута";
-    }
+  if (isSuccessful) {
+    cout << "Необходимая точность достигнута";
+  } else {
+    cout << "Необходимая точность не достигнута";
+  }
 
-    cout << "\n" << minValue << "\n" << point.x1 << " " << point.x2;
+  cout << "\n" << minValue << "\n" << point.x1 << " " << point.x2;
 }
